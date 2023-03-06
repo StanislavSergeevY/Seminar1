0dd627ed2e68f345a9b482b08fd53556bd37bf76
Stanislav Sergeev <126112002+StanislavSergeevY@users.noreply.github.com>
2023-03-07 01:37:23 +0300

    add common_task002_Проверка двух чисел на квадрат

---
+++ common_task002/Program.cs
@@ -0,0 +1,18 @@
+﻿//задача №1. Напишите программу, которая на вход принимает два числа и
+//проверяет, является ли первое число квадратом второго.
+
+//int a = 25, b = 5;
+
+Console.Write("Please add first volue: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please add second volue: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a / b == b)
+{
+    Console.Write("Yes! This is square!");
+}
+else
+{
+    Console.Write("No! This is no square!");
+}
