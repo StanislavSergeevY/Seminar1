6c5eb57a822a1e410bf2459f4ff74a99bd5dff00
Stanislav Sergeev <126112002+StanislavSergeevY@users.noreply.github.com>
2023-03-07 00:40:57 +0300

    common_task001_Квадрат числа

---
+++ common_task001/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу, которая на вход
+//принимает число и выдаёт его квадрат (Число умноженное на само себя)
+
+Console.Write("Please add to value: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат числа: ");
+Console.Write(n);
+Console.Write(" = ");
+Console.Write(n * n);
