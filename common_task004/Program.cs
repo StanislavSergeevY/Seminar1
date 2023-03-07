1195fcd922b95c74008a8d5fe2923575c9b80fef
Stanislav Sergeev <126112002+StanislavSergeevY@users.noreply.github.com>
2023-03-07 22:57:54 +0300

    add common_task004 N от -N до N

---
+++ common_task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №5. напишите программу, которая на вход принимает одно число(N), а на выходе
+//показывает целые числа в промежутке от -N до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = -n; i <= n; i++)
+{
+    Console.Write($"{i} ");
+}
